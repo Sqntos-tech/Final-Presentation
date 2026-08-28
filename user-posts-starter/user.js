fbf6aed808bb023e6f01a6d07c72607592b2779f
Sqntos-tech <edwardsantos1237@gmail.com>
2026-08-27 22:30:43 -0400

    update

--- user-posts-starter/user.js
+++ user-posts-starter/user.js
@@ -1,1 +1,29 @@
-console.log(localStorage.getItem("id"));
+const postListEl = document.querySelector(' .post-list')
+
+const id = localStorage.getItem("id")
+
+async function onSearchChange(event) {
+  const id = event.target.value
+  renderPosts(id)
+    
+}
+
+async function renderPosts(id) {
+   const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=:id${id}`)
+   const postsData = posts.json()
+
+   postListEl.innerHTML = postsData.map(post => postHTML(post)) .join("")
+}
+
+function postHTML(post) {
+  return `<div class="post">
+      <div class="post__title">
+        ${post.title}
+      </div>
+      <p class="post__body">
+        ${post.body}
+      </p>
+    </div>`
+}
+
+renderPosts()
